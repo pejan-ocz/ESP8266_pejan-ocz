69482849cb9b3d48545cacdc767e0f26067fef60
v-pejano <v-pejano@microsoft.com>
2017-02-19 13:30:27 +0100

    cleaning

---
+++ ESP8266_DS18b20/init.lua
@@ -0,0 +1,26 @@
+print(wifi.sta.getip())
+wifi.setmode(wifi.STATION)
+--wifi.sta.config("Linhart-TPLink","password")
+wifi.sta.config("JHW1","psw")
+
+-- wait for an IP
+cnt = 10
+tmr.alarm(0,500,1,function()
+  if wifi.sta.getip()==nil then
+    cnt = cnt-1
+    if cnt<=0 then
+
+      -- Did not get an IP in time, so quitting
+      tmr.stop(0)
+      --gpio.write(pin_error,1)
+      print "Not connected to wifi."
+    end
+  else
+
+    -- Connected to the wifi
+    tmr.stop(0)
+    print(wifi.sta.getip())
+    print("\nStarting main")
+    dofile("dallas2.lua")
+  end
+end)
