d85be997fc74597784d0f27186c12f948ebba300
pejan-ocz <pejan@outlook.cz>
2017-02-19 19:08:35 +0100

    update

--- ESP8266_DS18b20/dallas2.lua
+++ ESP8266_DS18b20/dallas2.lua
@@ -34,7 +34,7 @@ function sendData()
     conn:on("receive", function(conn, payload) print(payload) end)
     -- api.thingspeak.com 184.106.153.149
     conn:connect(80,'184.106.153.149') 
-    conn:send("GET /update?key=HLC2ABCCSWA584K1&field2="..t1.." HTTP/1.1\r\n") 
+    conn:send("GET /update?key=ThingSpeakAPIKey&field2="..t1.." HTTP/1.1\r\n") 
     conn:send("Host: api.thingspeak.com\r\n") 
     conn:send("Accept: */*\r\n") 
     conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
